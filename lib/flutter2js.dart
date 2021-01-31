81f1c93bbf460bd3144dd9a65839c9fe3340475c
JeffreyCao <jeffreycao1024@gmail.com>
2021-01-31 22:51:08 +0800

    flutter2js

--- lib/flutter2js.dart
+++ lib/flutter2js.dart
@@ -1,17 +1,72 @@
+import 'dart:convert';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
 
+// https://github.com/flutter/plugins/tree/master/packages/webview_flutter
 class Flutter2JsPage extends StatelessWidget {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
   @override
   Widget build(BuildContext context) {
+    AppBar appBar = AppBar(
+      title: Text('flutter -> js'),
+    );
+    final top = MediaQuery.of(context).padding.top;
+    final bottom = MediaQuery.of(context).padding.bottom;
+    final screenHeightAll = MediaQuery.of(context).size.height;
+    final screenHeight =
+        screenHeightAll - top - bottom - appBar.preferredSize.height;
+    print('screenHeight:$screenHeight');
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("flutter -> js"),
+        title: Text("JavaScript"),
       ),
-      body: Center(
-        child: Text("flutter2js"),
-      ),
+      body: Column(children: [
+        SizedBox(
+            child: WebView(
+              initialUrl: "",
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _loadHtmlFromAssets(webViewController);
+                _controller.complete(webViewController);
+              },
+            ),
+            height: screenHeight - 100),
+        SizedBox(
+            child: Center(
+                child: FlatButton(
+                    color: Colors.orange[200],
+                    onPressed: () {
+                      _sendMessage();
+                    },
+                    child: Text("发送数据给JS"))),
+            height: 100)
+      ]),
     );
+  }
+
+  _sendMessage() {
+    print('_sendMessage');
+    _controller.future.then((controller) {
+      print('eva');
+      controller
+          // .evaluateJavascript("alert('hi');")
+          .evaluateJavascript("alert('hi');")
+          .then((result) {
+        print("result: $result ");
+      }).catchError((error) => {print("error:$error")});
+    });
+  }
+
+  _loadHtmlFromAssets(webViewController) async {
+    String fileText = await rootBundle.loadString('html/flutter2js.html');
+    String content = Uri.dataFromString(fileText,
+            mimeType: 'text/html', encoding: Encoding.getByName('utf-8'))
+        .toString();
+    print(content);
+    webViewController.loadUrl(content);
   }
 }
